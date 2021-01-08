7e776a51f622ff942b81e9156e7a6a649e52ddf1
Alan <alansimon816@gmail.com>
2021-01-08 18:02:24 -0500

    Adds HTML screen divs and skeleton JS functions.

---
+++ script.js
@@ -0,0 +1,13 @@
+// A module that modifies the display.
+let displayController = (() => {
+
+})()
+
+// A module representing the game board. The game board is a 3x3 grid
+let gameBoard = (() => {
+    let gameBoard = []
+})()
+
+// A factory function for creating players.
+let Player = () => {
+}
